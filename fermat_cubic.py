9a4f5bccdc5afcd4d5cd893e574548102ae53fb8
Kevin <kevinliu04@gmail.com>
2023-05-27 14:22:51 -0400

    testing

---
+++ fermat_cubic.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import plotly.graph_objects as go
+
+plt.style.use("_mpl-gallery")
+
+# Make data
+X = np.arange(-5, 5, 0.25)
+Y = np.arange(-5, 5, 0.25)
+X, Y = np.meshgrid(X, Y)
+Z = np.cbrt(-(X**3) - Y**3 + 1)
+
+fig = go.Figure(data=[go.Surface(x=X, y=Y, z=Z)])
+
+# Set layout and axis labels
+fig.update_layout(
+    title="Diagram",
+    scene=dict(
+        xaxis_title="X",
+        yaxis_title="Y",
+        zaxis_title="Z",
+    ),
+)
+
+# Save plot as HTML file
+fig.write_html(
+    "fermat_cubic_plot.html",
+    include_plotlyjs="cdn",
+    include_mathjax="cdn",
+    full_html=False,
+)
+fig.show()
