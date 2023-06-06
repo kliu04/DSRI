478e93f5593c22a738585fc28a1f896daac8d627
Kevin <kevinliu04@gmail.com>
2023-06-06 09:55:48 -0400

    brief calculation of numbers and info page

--- custom_equation.js
+++ custom_equation.js
@@ -1,6 +1,8 @@
 "use strict";
 function handleSubmit(event) {
+    // base string
     let uri = "https://c3d.libretexts.org/CalcPlot3D/index.html?type=";
+    // check what kind of equation it is
     if (z.checked) {
         uri += "z;z=" + encodeURI(equation_input.value);
     }
@@ -10,18 +12,23 @@ function handleSubmit(event) {
     else if (implicit.checked) {
         uri += "implicit;equation=" + encodeURI(equation_input.value.replace("=", "~")) + ";cubes=16;visible=true;";
     }
+    // clear the text submission box
     equation_input.value = "";
-    console.log(uri);
-    event.preventDefault();
+    // make new iframe to open calcplot3d
     let iframe = document.createElement("iframe");
     iframe.src = uri;
     iframe.width = "1200";
     iframe.height = "1000";
+    // add it to the HTML and clear existing frames (if any)
     const node = document.getElementById("iframe");
     node.innerHTML = "";
     node.appendChild(iframe);
+    // stop page refresh
+    event.preventDefault();
 }
 function changePlaceholder() {
+    // format for equations
+    // TODO: more robust syntax explanations
     if (z.checked) {
         equation_input.placeholder = "z=";
     }
@@ -35,6 +42,7 @@ function changePlaceholder() {
         equation_input.placeholder = "Select an equation type";
     }
 }
+// HTML elements on page
 let z = document.getElementById("z");
 let parametric = document.getElementById("parametric");
 let implicit = document.getElementById("implicit");
