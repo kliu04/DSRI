06472fa2abadbfa0b3b005f36d05f7d0eac0169b
Kevin <kevinliu04@gmail.com>
2023-06-11 16:42:18 -0400

    solve singularities

---
+++ solve_singularities/solve_singularities.py
@@ -0,0 +1,75 @@
+from sympy import solve
+from sympy.abc import x, y, z
+
+from pathlib import Path
+
+
+def main():
+    # TODO: fix for server directory
+    path = Path("solve_singularities")
+    sols = []
+    with open(path / "file.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            # Get rid of useless characters
+            line = line.strip("\n")
+            line = line.replace("{", "")
+            line = line.replace("}", "")
+            line = line.replace(" ", "")
+            line = line.replace("^", "**")
+            tokens = line.split(",")
+            if len(tokens) == 2:
+                if "x" not in tokens:
+                    missing = x
+                elif "y" not in tokens:
+                    missing = y
+                elif "z" not in tokens:
+                    missing = z
+                system = []
+                system.append(eval(tokens[0]))
+                system.append(eval(tokens[1]))
+                sol = solve(
+                    system,
+                    # don't solve for the missing variable
+                    list({x, y, z} - {missing}),
+                    dict=True,
+                )
+                sol[0][missing] = 1
+                sols.append(sol)
+            elif len(tokens) == 3:
+                system = []
+                system.append(eval(tokens[0]))
+                system.append(eval(tokens[1]))
+                system.append(eval(tokens[2]))
+                sols.append(
+                    solve(
+                        system,
+                        [x, y, z],
+                        dict=True,
+                    )
+                )
+            else:
+                raise NotImplementedError
+
+    # flatten list
+    sols = [item for sublist in sols for item in sublist]
+
+    with open(path / "solutions.txt", "w", encoding="utf-8") as f:
+        # for item in sols:
+        #     sol = []
+        #     sol.append(item[x])
+        #     sol.append(item[y])
+        #     sol.append(item[z])
+        #     f.write(str(sol) + "\n")
+
+        for i, v in enumerate(sols):
+            sol = (v[x], v[y], v[z])
+            sols[i] = sol
+
+        # Remove duplicates
+        sols = list(dict.fromkeys(sols))
+        for sol in sols:
+            f.write(str(sol) + "\n")
+
+
+if __name__ == "__main__":
+    main()
