1a7c7cde0db2a9dd168384ed49ba80eb1b8551b5
Kevin <kevinliu04@gmail.com>
2023-06-20 17:52:34 -0400

    removed incorrect code

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-touch solutions.txt
-M2 --script solve_all.m2 $1 > solutions.txt;
-python3 solve_singularities.py;
-python3 solve_multiplicities.py;
-cat sings_solution.txt >> solutions.txt;
-M2 --script solve_all2.m2 >> solutions.txt;
-# delete warnings
-sed -i '/--/d' solutions.txt;
