f829a56b2c606a67f0721772c6099a7599b5317f
Kevin Liu <kevinliu04@gmail.com>
2023-06-25 19:22:12 -0400

    dark mode persist

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  let mode = localStorage.getItem("mode");
+  if (mode === null) {
+    localStorage.setItem("mode", "dark");
+  }
+});
+
 document.getElementById("btnSwitch").addEventListener("click", () => {
-  if (document.documentElement.getAttribute("data-bs-theme") == "dark") {
+  // if (document.documentElement.getAttribute("data-bs-theme") == "dark") {
+  //   document.documentElement.setAttribute("data-bs-theme", "light");
+  // } else {
+  //   document.documentElement.setAttribute("data-bs-theme", "dark");
+  // }
+
+  if (localStorage.getItem("mode") === dark) {
     document.documentElement.setAttribute("data-bs-theme", "light");
   } else {
     document.documentElement.setAttribute("data-bs-theme", "dark");
