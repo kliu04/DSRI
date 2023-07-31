b16fc93e93d638fdb20dfa5c5cec644db7428fc1
Wevie0 <kevinliu04@gmail.com>
2023-07-30 22:38:14 -0400

    1.0.0

--- math/3D_solvers/run_solvers.py
+++ math/3D_solvers/run_solvers.py
@@ -1,14 +1,22 @@
 import json
 import wrapper
 from parse import parse
+import signal
+
+
+def timeout(signum, frame):
+    raise TimeoutError
 
 
 def main():
     surfaces = []
+    # timeout if computations take too long
+    signal.signal(signal.SIGALRM, timeout)
     with open("math/3D_solvers/surfaces.json", "r") as f:
         # Load title and eqn
         surfaces = json.load(f)
         for i, surface in enumerate(surfaces):
+            signal.alarm(60)
             try:
                 # parse eqn if needed
                 parsed = parse(surface["eqn"])
@@ -21,9 +29,12 @@ def main():
                 surface["sings"] = str(surface["sings"])
                 # update list
                 surfaces[i] = surface
+            except TimeoutError:
+                print("Skipping " + surface["title"])
+                continue
             except Exception as e:
                 print(e, surface)
-
+    signal.alarm(0)
     with open("math/3D_solvers/surfaces.json", "w") as f:
         json.dump(surfaces, f, indent=4)
 
